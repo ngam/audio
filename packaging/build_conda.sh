626aae6821673018973560f1bb413e4d1bee6ffd
ngam <67342040+ngam@users.noreply.github.com>
2021-11-08 10:45:07 -0500

    edits

--- packaging/build_conda.sh
+++ packaging/build_conda.sh
@@ -14,6 +14,8 @@ setup_visual_studio_constraint
 # need to change somethings
 export CONDA_EXTRA_BUILD_CONSTRAINT="- openblas"
 export CONDA_PYTORCH_CONSTRAINT="- pytorch==1.10.0"
+export CONDA_PYTORCH_BUILD_CONSTRAINT="- pytorch==1.10.0"
+export BUILD_VERSION="0.10.0"
 # nvidia channel included for cudatoolkit >= 11
 # add conda-forge and fastchan
-conda build -c conda-forge -c fastchan -c defaults -c nvidia $CONDA_CHANNEL_FLAGS --no-anaconda-upload --python "$PYTHON_VERSION" packaging/torchaudio
+conda build -c conda-forge -c fastchan -c defaults --no-anaconda-upload --python "$PYTHON_VERSION" packaging/torchaudio
