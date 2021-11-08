67f9fdfa746d7d1069bd99cab919d589f9421a25
ngam <67342040+ngam@users.noreply.github.com>
2021-11-08 09:52:31 -0500

    add openblas, del mkl, add -c conda-forge, fastchan

--- packaging/build_conda.sh
+++ packaging/build_conda.sh
@@ -5,11 +5,15 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . "$script_dir/pkg_helpers.bash"
 
 export BUILD_TYPE="conda"
-setup_env 0.11.0
+setup_env 0.10.0
 export SOURCE_ROOT_DIR="$PWD"
 setup_conda_pytorch_constraint
 setup_conda_cudatoolkit_constraint
 setup_visual_studio_constraint
+
 # need to change somethings
+export CONDA_EXTRA_BUILD_CONSTRAINT="openblas"
+
 # nvidia channel included for cudatoolkit >= 11
-conda build -c defaults -c nvidia $CONDA_CHANNEL_FLAGS --no-anaconda-upload --python "$PYTHON_VERSION" packaging/torchaudio
+# add conda-forge and fastchan
+conda build -c conda-forge -c fastchan -c defaults -c nvidia $CONDA_CHANNEL_FLAGS --no-anaconda-upload --python "$PYTHON_VERSION" packaging/torchaudio
